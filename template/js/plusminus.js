581142be1025b09bfe9e406ba36e42fdb7d6486f
nataerm251080 <nataerm251080@gmail.com>
2016-08-15 15:28:10 +0000

    Первая версия сайта

---
+++ template/js/plusminus.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Ermakova on 15.08.2016.
+ */
+jQuery(document).ready(function($) {
+    $('.quont-minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.quont-plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+});
---
+++ views/layouts/header.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>Море икры</title>
+
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="robots" content="index, follow" />
+        <meta name="keywords" content="икра, доставка икры, море икры" />
+        <meta name="description" content="Икра и морепродукты с доставкой на дом. Условия доставки икры в ваш город. Море икры" />
+        <link href="http://fonts.googleapis.com/css?family=Arial:400,300,100,400italic,900,700,500|Arial+Condensed:400,300,700&subset=latin,cyrillic-ext" type="text/css"  rel="stylesheet" />
+        <link href="http://fonts.googleapis.com/css?family=Forum&subset=latin,cyrillic-ext" type="text/css"  rel="stylesheet" />
+         <link href="/template/css/bootstrap.min.css" rel="stylesheet">
+        <link href="/template/css/font-awesome.min.css" rel="stylesheet">
+        <link href="/template/css/prettyPhoto.css" rel="stylesheet">
+        <link href="/template/css/price-range.css" rel="stylesheet">
+        <link href="/template/css/animate.css" rel="stylesheet">
+        <link href="/template/css/main.css" rel="stylesheet">
+        <link href="/template/css/responsive.css" rel="stylesheet">
+
+
+        <!--[if lt IE 9]>
+        <script src="js/html5shiv.js"></script>
+        <script src="js/respond.min.js"></script>
+
+
+        <![endif]-->       
+        <link rel="shortcut icon" href="/template/images/ico/favicon.ico">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/template/images/ico/apple-touch-icon-144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/template/images/ico/apple-touch-icon-114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/template/images/ico/apple-touch-icon-72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="/template/images/ico/apple-touch-icon-57-precomposed.png">
+    </head><!--/head-->
+
+    <body>
+        <div class="page-wrapper">
+
+
+            <header id="header"><!--header-->
+
+                <!-- <div class="header_top"><!--header_top-->
+                <!--  <div class="container">
+                     <div class="row">
+                         <div class="col-sm-6">
+                             <div class="contactinfo">
+                                 <!-- <ul class="nav nav-pills">
+                                     <li><a href="#"><i class="fa fa-phone"></i> </a></li>
+                                     <li><a href="#"><i class="fa fa-envelope"></i> </a></li>
+                                 </ul> -->
+                <!--  </div>
+             </div>
+             <div class="col-sm-6">
+                 <div class="social-icons pull-right">
+                     <ul class="nav navbar-nav">
+                         <li><a href="#"><i class="fa fa-facebook" target="_blank"></i></a></li>
+                         <li><a href="#"><i class="fa fa-google-plus" target="_blank"></i></a></li>
+                         <li><a href="#"><i class="fa fa-vk" target="_blank"></i></a></li>
+                         <li><a href="#"><i class="fa fa-twitter" target="_blank"></i></a></li>
+                     </ul>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div><!--/header_top-->
+
+                <div class="header-middle"><!--header-middle-->
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <div class="logo pull-left">
+                                    <a href="/"><img src="/template/images/home/logo.png" alt="" /></a>
+                                    <a href="/"><div class="text">Море икры</div></a>
+                                     </div>
+                            </div>
+
+
+                            <div class="col-sm-8">
+                                <div class="shop-menu pull-right">
+
+
+
+                                    <ul class="nav navbar-nav">
+                                        <li><a href="/cart">
+                                                <i class="fa fa-shopping-cart"></i> Корзина 
+                                                (<span id="cart-count"><?php echo Cart::countItems(); ?></span>)
+                                            </a>
+                                        </li>
+                                        <?php if (User::isGuest()): ?>                                        
+                                            <li><a href="/user/login/"><i class="fa fa-lock"></i> Вход</a></li>
+                                        <?php else: ?>
+                                            <li><a href="/cabinet/"><i class="fa fa-user"></i> Аккаунт</a></li>
+                                            <li><a href="/user/logout/"><i class="fa fa-unlock"></i> Выход</a></li>
+                                        <?php endif; ?>
+                                    </ul>
+                                </div>
+                            </div>
+
+                      <div class="social-icons pull-right">
+                        <ul class="nav navbar-nav">
+                            <li><a href="#"><i class="fa fa-facebook" target="_blank"></i></a></li>
+                            <li><a href="#"><i class="fa fa-google-plus" target="_blank"></i></a></li>
+                            <li><a href="#"><i class="fa fa-vk" target="_blank"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter" target="_blank"></i></a></li>
+                        </ul>
+                    </div>
+
+                    <div class="contact">
+                        <p class="text-right"> телефон   +7(495) 92 87 12<br> сот. 7(299) 324 56 87</p>
+                    </div>
+                </div>
+        </div><!--/header-middle-->
+                </div><!--/header-middle-->
+
+                <div class="header-bottom"><!--header-bottom-->
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="navbar-header">
+                                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                                        <span class="sr-only">Toggle navigation</span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                    </button>
+                                </div>
+
+                                <div class="mainmenu pull-left">
+                                    <ul class="nav navbar-nav collapse navbar-collapse">
+                                        <li><a href="/">Главная</a></li>
+                                        <li class="dropdown"><a href="#">Магазин<i class="fa fa-angle-down"></i></a>
+                                            <ul role="menu" class="sub-menu">
+                                                <li><a href="/catalog/">Каталог товаров</a></li>
+                                                <li><a href="/cart/">Корзина</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="/dostavka/">Доставка и оплата</a></li>
+                                        <li><a href="/about/">О магазине</a></li>
+                                        <li><a href="/contacts/">Контакты</a></li>
+                                        <li><a href="/news/">Новости</a></li>
+
+                                    </ul>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div><!--/header-bottom-->
+
+            </header><!--/header-->
